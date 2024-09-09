b4e17f26c12d4a9f3e41fcc7646190e27a0684bd
Hans Pagel <mail@hanspagel.com>
2024-09-09 15:58:02 +0200

    feat: add all configuration options

--- resources/views/reference.blade.php
+++ resources/views/reference.blade.php
@@ -11,6 +11,15 @@
     <script
       id="api-reference"
       data-url="{{ config('scalar.url') }}"></script>
+
+    <!-- Optional: You can set a full configuration object like this: -->
+    <script>
+        var configuration = {!! json_encode(config('scalar.configuration')) !!}
+
+        document.getElementById('api-reference').dataset.configuration =
+            JSON.stringify(configuration)
+    </script>
+
     <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
   </body>
 </html>
