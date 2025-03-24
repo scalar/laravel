e70bd9e7b9c17f40a6eb94cf56b26b4ca4c26478
Hans Pagel <mail@hanspagel.com>
2025-03-24 12:50:19 +0100

    feat: use new JS API

--- config/scalar.php
+++ config/scalar.php
@@ -89,34 +89,45 @@ return [
         // 'none',
         /** The layout to use for the references */
         'layout' => 'modern',
+
         /** URL to a request proxy for the API client */
-        'proxy' => 'https://proxy.scalar.com',
+        'proxyUrl' => 'https://proxy.scalar.com',
+
         /** Whether to show the sidebar */
         'showSidebar' => true,
+
         /**
          * Whether to show models in the sidebar, search, and content.
          */
         'hideModels' => false,
+
         /**
          * Whether to show the “Download OpenAPI Document” button
          */
         'hideDownloadButton' => false,
+
         /**
          * Whether to show the “Test Request” button
          */
         'hideTestRequestButton' => false,
+
         /**
          * Whether to show the sidebar search bar
          */
         'hideSearch' => false,
+
         /** Whether dark mode is on or off initially (light mode) */
         'darkMode' => false,
+
         /** forceDarkModeState makes it always this state no matter what*/
         'forceDarkModeState' => 'dark',
+
         /** Whether to show the dark mode toggle */
         'hideDarkModeToggle' => false,
+
         /** Key used with CTRL/CMD to open the search modal (defaults to 'k' e.g. CMD+k) */
         'searchHotKey' => 'k',
+
         /**
          * If used, passed data will be added to the HTML header
          *
@@ -125,12 +136,14 @@ return [
         'metaData' => [
             'title' => config('app.name').' API Reference',
         ],
+
         /**
          * Path to a favicon image
          *
          * @example '/favicon.svg'
          */
         'favicon' => '',
+
         /**
          * List of httpsnippet clients to hide from the clients menu
          * By default hides Unirest, pass `[]` to show all clients
@@ -144,31 +157,35 @@ return [
             'clientKey' => 'curl',
         ],
         /** Custom CSS to be added to the page */
-        'customCss' => '',
+        // 'customCss' => '',
+
         /** Prefill authentication */
-        'authentication' => [
-            // TODO
-        ],
+        // 'authentication' => [
+        //     // TODO
+        // ],
+
         /**
          * The baseServerURL is used when the spec servers are relative paths and we are using SSR.
          * On the client we can grab the window.location.origin but on the server we need
          * to use this prop.
          */
-        'baseServerURL' => '',
+        // 'baseServerURL' => '',
+
         /**
          * List of servers to override the openapi spec servers
          */
-        'servers' => null,
         // 'servers' => [
         //     [
         //         'url' => 'https://api.scalar.com',
         //         'description' => 'Production server',
         //     ],
         // ],
+
         /**
          * We’re using Inter and JetBrains Mono as the default fonts. If you want to use your own fonts, set this to false.
          */
         'withDefaultFonts' => true,
+
         /**
          * By default we only open the relevant tag based on the url, however if you want all the tags open by default then set this configuration option :)
          */
